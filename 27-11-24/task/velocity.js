12e2ef18f01048795f5e33ffe2e9260743ab6c97
aitji <ait.suriya@gmail.com>
2024-11-27 20:25:31 +0700

    Update velocity.js

--- 27-11-24/task/velocity.js
+++ 27-11-24/task/velocity.js
@@ -1,8 +1,8 @@
 import { input } from "../../function.js"
 
 (async () => {
-    const veclocity = await input('ป้อนความเร็ว : ')
-    if (veclocity > 120) console.log("ปรับ 1,000 บาท")
-    else if (veclocity > 80) console.log("ปรับ 500 บาท")
+    const velocity = await input('ป้อนความเร็ว : ')
+    if (velocity > 120) console.log("ปรับ 1,000 บาท")
+    else if (velocity > 80) console.log("ปรับ 500 บาท")
     else console.log('ไม่มีการปรับเงิน')
 })()
